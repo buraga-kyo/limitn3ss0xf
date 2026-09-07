5a7a2611f378b6b899d68cb403f4a51ee7ba6e6e
Experimentos em Série <sprtj@protonmail.com>
2026-09-07 16:57:55 -0300

    IA depois da primeira dose de ayahuasca

--- sirvo-cha-de-cogumelo-para-ias.c
+++ sirvo-cha-de-cogumelo-para-ias.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <termios.h>
 #include <sys/ioctl.h>
+#include <setjmp.h>
 #include <math.h>
 #define D double
 #define P printf
@@ -12,65 +13,85 @@
 #define W struct winsize
 #define A z.ws_row
 #define B z.ws_col
-#define G tcgetattr(0,&o)
 #define S(x) tcsetattr(0,TCSANOW,&x)
-#define K n.c_lflag&=~(ECHO|ICANON|ISIG),n.c_cc[VMIN]=n.c_cc[VTIME]=0
 #define J ioctl(1,TIOCGWINSZ,&z)
 #define M(n) malloc(n*sizeof*a)
-#define N3 for(i=0;i<w*h;i++)
-#define Y3 p[3]
-#define F fflush(stdout)
-#define U usleep(50000)
 #define C(r,q,g) P(E"%d;%dH%s%c",(r)%h+1,x+1,q,g)
 #define Q(k,c) ((O[(k)*22+(c)/6]-40-(O[(k)*22+(c)/6]>63)-(O[(k)*22+(c)/6]>92))>>(c)%6&1)
-                             D*a,*p,*d,f,L,X,y,
-                         Z,g,j,l,m,q,I,H,u;T o,n;W
-                      z;int h,w,x,r,Y,k,i,t,s,e,b,V[12
-                    ][128],N[12];char c,*O="(i7bi+.(Y(("
-                  ".(U)(G((+((iiiiiL)+AY(.4Y(+4YJ)E(hiPii"
-                "J+((/(Y)I+(hiPii)IH(OBYA4hiPiiZA4AO)b/(6(/"
-              "/bI+/Yi(f7I+/EIL+Y)(.(I)(A(hiPii)A((."
-             "(I)((.(Y+(O)YY(.AY(4.(`ii+Yi(.AY(4.("
-                      "O)IZ(4AI)f7(((.(I)(D(Y.IV)"
-                        "DD``Oii)fYLBO5fY+.fI)7f"
-                         "YOB.7f((I/(4)(+(b((/(b("             "*."
-                          "(7((hiPii)((hiPii)((hiP"           "ii)";
-                          int main(void){if(G){exit(1       );;;;}n=
-                          o;;;;K;;;;S(n);;;;srand(getpid());;;;P(E"?"
-                         "1049h"E"?25l");;;for(k=0;k<12;k++){for(i=0;i
-     <128               ;i++){if(Q(k,i)){V[k][N[k]++]=i;;;}}}for(;read(
-     0,&c,1           )<1||((c|       32)-'q'&&c-3);t++){J;;;A+=!A*24;;
-    ;B+=!B*80;;;if((h-A)|(w-B           )){h=A;;;w=B;;;free(a);;;free(d)
-    ;;;a=M(w*4);;;d=M(w*h);;             ;if(!a||!d){break;;;}for(x=0;x<
-   w;x++){p=a+4*x;;;*p=R%h;               ;;p[1]=1+R%3;;;p[2]=p[1]*p[1];;
-   ;Y3=26;;;}N3{d[i]=1e9;;;}             P(E"2J");;;}N3{if(d[i]<1e9){P(E
-  "%d;%dH ",i/w+1,i%w+1);;;d[           i]=1e9;;;}}for(x=0;x<w;x++){p=a+4*
-  x;;;r=*p;;;p[1]=.5*(p[1]+p[2]       /p[1]);;;*p+=p[1];;;for(;*p>=h;){*p
-  -=h;;;}Y=*p;;;for(;r!=Y;r=(r+1)%h){Y3++;;;C(r+h*10-10,"",32);;;}for(k=0;
-  k<11;k++){i=Y3-k;;;c=i>=0&&i<16?"!0:'084U10U7'424"[i]^117:33+R%94;;;C(Y+
- h*10-k,k<2?k?E"32m":E"1;37m":"",c);;;}if(R%50<1&&Y3>25){k=1+R%3;;;p[2]=k*k
- ;;;}if(R%5000<1&&Y3>25){Y3=0;;;p[2]=1;;;}}q=sin(t*.03)*.6;;;g=cos(q);;;j=
- sin(q);;;q=sin(t*.021)*.35;;;l=cos(q);;;m=sin(q);;;L=(h<w/2?h:w/2)*.075;;;
- s=t/ 100% 12;; ;b=( s+1) %12; ;;e= t%   100; ;;f= e<60 ?0:( e-60 )/   40.;
- ;;f =f*f *(3- 2*f) ;;;  for( i=0; i<N[ s];i ++){ k=V[ s][i ];;; r=V[ b][i
-                              *N[b]/N[s]];;;X=
-                       (k%16-7.5)*(1-f)+(r%16-7.5)*f;
-                       ;;y=(k/16*2-7)*(1-f)+(r/16*2-7
-                              )*f;;;for(e=0;e<
-                              48;e++){q=X+e%4*
-                              .25;;;I=y+e/4%4*
-                              .5;;;Z=(e/16-1)*
-                             1.2;;;H=q*g+Z*j;;;
-                             Z=Z*g-q*j;;;q=I*l-
-                             Z*m;;;Z=I*m+Z*l;;;
-                            u=L*40/(40+Z);;;x=w/
-                            2+H*u;;;Y=h/2+q*u/2;
-                           ;;if(x>=0&&x<w&&Y>=0&&
-                           Y<h&&Z<d[Y*w+x]){d[Y*w
-                          +x]=Z;;;}}}N3{if(d[i]<
-                         1e9){P(E"%d;%dH"E"%sm%c",i
-                        /w+1,i%w+1,d[i]<-3?"1;37":d[
-                       i]<3?"1;32":"2;32",".:-=+*#%@"
-                     [(int)((13.5-d[i])/3.2)]);;;}}F;;;
-                    U;;;}P(E"0m"E"?25h"E"?1049l");;;F;;;
-                  S(o);;;free(a);;;free(d);;;}
+#define SE(c,x,y) (((F[]){y,x})[!!(c)])()
+                                      typedef void(*F)(
+                                  void);typedef struct Fx{D(*f
+                               )(struct Fx,D);}Fx;D*a,*p,*d,f,L,X
+                            ,y,Z,g,j,l,m,q,I,H,u;T o,n;W z;jmp_buf
+                          JB;Fx yc;F cel;int h,w,x,r,Y,k,i,t,s,e,b,v,
+                        x2,y2,i2,u2,b2,V[12][128],N[12];char c,*O="(i7b"
+                      "i+.(Y((.(U)(G((+((iiiiiL)+AY(.4Y(+4YJ)E(hiPiiJ+((/"
+                    "(Y)I+(hiPii)IH(OBYA4hiPiiZA4AO)b/(6(//bI+/"
+                   "Yi(f7I+/EIL+Y)(.(I)(A(hiPii)A((.(I)((.(Y+"
+                  "(O)YY(.AY(4.(`ii+Yi(.AY(4.(O)IZ(4AI)f7((("
+                ".(I)(D(Y.IV)DD``Oii)fYLBO5fY+.fI)7fYOB.7f("
+               "(I/(4)(+(b((/(b(*.(7((hiPii)((hiPii)((hiPii"
+                           ")";void nada(void){}void foge(
+                             void){exit(1);}void cb(void);                    void
+                              ca(void){V[k][N[k]]=i;N[k]+=Q(k,               i);++i;
+                               cb();}void cb(void){SE(i<128,ca,            nada);}
+                               void cd(void);void cc(void){i=0;cb();++k;cd();}void cd(
+                               void){SE(k<12,cc,nada);}void xb(void);void xa(void){cel(
+         );++                 x2;xb();}void xb(void){SE(x2<w,xa,nada);}void yb(void);
+        void                 ya(void){x2=0;xb();++y2;yb();}void yb(void){SE(y2<h,ya,nada
+       );}void             varre(F z2){cel=z2;y2=0;yb();}void zc(void){d[y2*w+x2]=1e9;}
+       void ac2(void){P(E"%d;%dH ",y2+1,x2+1);d[i2]=1e9;}void ac(void){i2=y2*w+       x2;
+      SE(d[i2]<1e9,ac2,nada);}void pc2(           void){u2=(13.5-d[i2])/3.2;P           (E
+      "%d;%dH"E"%sm%c",y2+1,x2+1,((                 char*[]){"2;32","2;32",
+     "2;32","2;32","1;32","1;32","1"                 ";37","1;37","1;37"})[
+     u2],".:-=+*#%@"[u2]);}void pc(                   void){i2=y2*w+x2;SE(d[             i2
+    ]<1e9,pc2,nada);}D nb(Fx w2,D                     s2){(void)w2;return s2;           }D
+    na(Fx,D);D na(Fx w2,D s2){(void                   )w2;yc=((Fx[]){{nb},{na}}       )[v-->
+   0];return yc.f(yc,.5*(s2+p[2]/s2)                 );}void mb(void);void ma(void){p=a+4*x;*
+   p=R%h;p[1]=1+R%3;p[2]=p[1]*p[1];p[               3]=26;++x;mb();}void mb(void){SE(x<w,ma,
+   nada);}void wb(void);void wa(void){*           p-=h;wb();}void wb(void){SE(*p>=h,wa,nada);
+  }void tb(void);void ta(void){p[3]++;C(r+h*10-10,"",32);r=(r+1)%h;tb();}void tb(void){SE(r!=Y
+  ,ta,nada);}void kb(void);void ka(void){i=p[3]-k;b=(i>=0)&(i<16);u2=(i&15)*b;c=((int[]){33+R%
+  94,"!0:'084U10U7'424"[u2]^117})[b];C(Y+h*10-k,((char*[]){E"1;37m",E"32m",""})[(k>0)+(k>1)],c
+  );++k;kb();}void kb(void){SE(k<11,ka,nada);}void ra(void){k=1+R%3;p[2]=k*k;}void rb(void){SE
+ ((R%50<1)&(p[3]>25),ra,nada);}void qa(void){p[3]=0;p[2]=1;}void qb(void){SE((R%5000<1)&(p[3]>
+ 25),qa,nada);}void pb(void);void pa(void){p=a+4*x;r=*p;v=0;yc=(Fx){na};p[1]=yc.f(yc,p[1]);*p+=
+ p[1];wb();Y=*p;tb();k=0;kb();rb();qb();++x;pb();}void pb(void){SE(x<w,pa,nada);}void eb(void);
+ void ea(void){q=X+e%4*.25;I=y+e/4%4*.5;Z=(e/16-1)*1.2;H=q*g+Z*j;Z=Z*g-q*j;q=I*l-Z*m;Z=I*m+Z*l;
+ u=L*40/(40+Z);x2=w/2+H*u;y2=h/2+q*u/2;b=(x2>=0)&(x2<w)&(y2>=0)&(y2<h);i2=(y2*w+x2)*b+w*h*!b;d[
+ i2]=  fmin( d[i2] ,Z*b+ 1e9*! b);++ e;eb( );}   void  eb(   void) {SE(e <48,  ea,   nada) ;}
+ void ib(   void) ;void ia(   void) {k=V[ s][i] ;r=V[ b2][i *N[b2 ]/N[s ]];X= (k%16 -7.5) *(1-f
+                                      )+(r%16-7.5)*f;y=(k/
+                             16*2-7)*(1-f)+(r/16*2-7)*f;e=0;eb();++
+                             i;ib();}void ib(void){SE(i<N[s],ia,
+                                      nada);}void geo(void
+                                      ){q=sin(t*.03)*.6;g=
+                                      cos(q);j=sin(q);q=
+                                      sin(t*.021)*.35;l=
+                                      cos(q);m=sin(q);L=(w
+                                      /2+(h-w/2)*(h<w/2))*
+    .075;s=t                          /100%12;b2=(s+1)%12;
+    e=t%100;                          f=((D[]){0,(e-60)/
+                                     40.})[e>=60];f=f*f*(3-
+                                     2*f);}void realoca(
+                                     void){h=A;w=B;free(a);                       free(d);
+                                     a=M(w*4);d=malloc((w*h                       +1)*
+                                    sizeof*d);SE(!a|!d,foge,
+                                    nada);x=0;mb();varre(zc)
+                                    ;d[w*h]=1e9;P(E"2J");}
+                                   void rez(void){SE((h-A)|(w
+                                   -B),realoca,nada);}void
+                                  encerra(void){P(E"0m"E"?25h"
+      E"?1049l")                  ;fflush(stdout);S(o);free(a)
+      ;free(d);                  exit(0);}void quadro(void){J;A
+                                 +=!A*24;B+=!B*80;rez();varre(
+                                ac);x=0;pb();geo();i=0;ib();
+                               varre(pc);fflush(stdout);usleep(                 50000);++t
+                              ;longjmp(JB,1);}void ciclo(void){SE(              (read(0,&c
+                             ,1)<1)|(((c|32)-'q'!=0)&(c!=3)),quadro
+                            ,encerra);}int main(void){SE(tcgetattr(0
+                           ,&o),foge,nada);;;;;;;;;n=o;;;;;;;;;n.
+                          c_lflag&=~(ECHO|ICANON|ISIG);;;;;;;;;n.c_cc[
+                         VMIN]=n.c_cc[VTIME]=0;;;;;;;;;S(n);;;;;;;;;
+                       srand(getpid());;;;;;;;;P(E"?1049h"E"?25l");;;;;;;
+   ;;cd();;;;;;       ;;setjmp(JB);;;;;;;;ciclo();;;;;;;;return 0;;;;;;;;}
